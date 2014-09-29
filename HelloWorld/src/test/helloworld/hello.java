ecf81b5884b4442fa5d61fe5126b8d010712bb93
roneill1992 <roneill35@qub.ac.uk>
2014-09-29 16:26:24 +0100

    Ryan's hello
    Hello world

--- HelloWorld/src/test/helloworld/hello.java
+++ HelloWorld/src/test/helloworld/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello wurld");
 		System.out.println("hello from emma");
+		System.out.println("Hello from ryan");
 	}
 
 }
